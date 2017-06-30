685275617071de5b6b5e93637dede620219dbcf4
Yinzhe_Qi <qiyzh@neusoft.com>
2017-06-30 17:50:10 +0800

    look for config.yaml in path of yui.py(or yui.exe)

--- yui.py
+++ yui.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from yui_oss import Yui, VERSION
+import sys
+import os
 
 if __name__ == "__main__":
-    yui = Yui("config.yaml")
+    path = sys.path[0]
+    if os.path.isfile(path):
+        path = os.path.dirname(path)
+    yui = Yui(path + "/config.yaml")
     yui.run()
+
 
